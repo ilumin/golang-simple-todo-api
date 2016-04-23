e3586e07e6df7f2a4cfd0fe81e189b9bb3b84e46
Teerasak Vichadee <iolumin@gmail.com>
2016-04-23 13:57:21 +0700

    ADD todos, todos/1 handler

--- src/todoapp/main.go
+++ src/todoapp/main.go
@@ -12,10 +12,24 @@ import (
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoID}", TodoDetail)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-// Index request handler from "/"
+// Index request handler for "/"
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+// TodoIndex request handler for "/todos"
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Todo Index!")
+}
+
+// TodoDetail request handler for "/todos/{todoID}"
+func TodoDetail(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	todoID := params["todoID"]
+	fmt.Fprintln(w, "Todo detail:", todoID)
 }
