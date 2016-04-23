a36b50cbc78478ca0d05e85a1009e8669d6e48f9
Teerasak Vichadee <iolumin@gmail.com>
2016-04-23 14:31:17 +0700

    UPDATE use json alias for struct properties UPDATE change from Todo.Due to Todo.ExpiredAt

--- src/todoapp/todo.go
+++ src/todoapp/todo.go
@@ -4,9 +4,9 @@ import "time"
 
 // Todo a structure for todo object
 type Todo struct {
-	Name      string
-	Completed bool
-	Due       time.Time
+	Name      string    `json:"name"`
+	Completed bool      `json:"completed"`
+	ExpiredAt time.Time `json:"expired_at"`
 }
 
 // Todos a collection of Todo
