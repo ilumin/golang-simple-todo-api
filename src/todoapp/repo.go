f1b457104caffcd6cbb746d917ce6733d315da56
Teerasak Vichadee <iolumin@gmail.com>
2016-04-23 17:41:26 +0700

    ADD repository

---
+++ src/todoapp/repo.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+var currentID int
+
+var todos Todos
+
+func init() {
+	RepoCreateTodo(Todo{Name: "Learn GO!"})
+	RepoCreateTodo(Todo{Name: "Learn write API with GO!"})
+}
+
+// RepoCreateTodo create todo item
+func RepoCreateTodo(todo Todo) Todo {
+	currentID++
+	todo.ID = currentID
+	todos = append(todos, todo)
+	return todo
+}
+
+// RepoFindTodo find todo item
+func RepoFindTodo(id int) Todo {
+	for _, todo := range todos {
+		if todo.ID == id {
+			return todo
+		}
+	}
+
+	return Todo{}
+}
+
+// RepoDestroyTodo remove todo item
+func RepoDestroyTodo(id int) error {
+	for i, todo := range todos {
+		if todo.ID == id {
+			todos = append(todos[:i], todos[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("Cound not find Todo with id of %d to delete", id)
+}
--- src/todoapp/todo.go
+++ src/todoapp/todo.go
@@ -4,6 +4,7 @@ import "time"
 
 // Todo a structure for todo object
 type Todo struct {
+	ID        int       `json:"id"`
 	Name      string    `json:"name"`
 	Completed bool      `json:"completed"`
 	ExpiredAt time.Time `json:"expired_at"`
